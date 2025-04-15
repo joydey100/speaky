c37ff1cf64a0b5d97113beea8f2fee11addbfdd5
Joy <joydey.bd.2002@gmail.com>
2025-04-15 11:48:48 +0600

    socket new update

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -1,6 +1,9 @@
 import { Server } from "socket.io";
 import http from "http";
 import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
@@ -8,7 +11,8 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin:
+      process.env.NODE_ENV === "development" ? "http://localhost:5173" : "/",
     credentials: true,
   },
 });
