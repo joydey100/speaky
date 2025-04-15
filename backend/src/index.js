445b02a1779e8e109781641b014149d8edc4cdc3
Joy <joydey.bd.2002@gmail.com>
2025-04-16 00:38:09 +0600

    change layout and change express version

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,12 +6,17 @@ import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import cors from "cors";
 import { app, server } from "./lib/socket.js";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
 import path from "path";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
-const __dirname = path.resolve();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+console.log(__dirname);
 
 // Middlewares
 app.use(express.json({ limit: "10mb" }));
@@ -27,13 +32,11 @@ app.use(
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
-  app.get("/*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
-}
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../frontend", "dist", "index.html"));
+});
 
 server.listen(PORT, () => {
   connectDB();
